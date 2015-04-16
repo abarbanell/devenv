91e38f27ea32da8b2e622ee4a6bbc1cd6fa41f55
Tobias Abarbanell <tobias.abarbanell@frontiersin.org>
2015-04-16 16:40:01 +0200

    introduced docker-compose

--- bin/devenv.sh
+++ bin/devenv.sh
@@ -3,26 +3,22 @@
 SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
 cd $SCRIPT_HOME/..
 
-MODULES="mongo"
-
 case "$1" in
-       up)
-               boot2docker up
-							 for mod in $MODULES
-							 do
-								 $SCRIPT_HOME/devenv.${mod}.sh up
-							 done
-               ;;
-			 down)
-							 boot2docker down
-							 ;;
-			 shellinit)
-							 boot2docker shellinit
-							 ;;
-       update)
-               git pull
-               ;;
-       *)
-               echo usage: $0 "[up|dhellinit|update]"
-               ;;
+	up)
+		boot2docker up
+		docker-compose up -d
+		;;
+down)
+		docker-compose stop
+		boot2docker down
+		;;
+shellinit)
+		boot2docker shellinit
+		;;
+update)
+		git pull
+		;;
+*)
+		echo usage: $0 "[up|dhellinit|update]"
+		;;
 esac
